e39f270a611a7e0a3d52fdd6a56324b11a3c45ab
David Mosher <davidmosher@gmail.com>
2012-09-21 17:23:39 -0600

    spiffed things up with an ajax loading indicator

--- app/js/settings.js
+++ app/js/settings.js
@@ -4,4 +4,18 @@
     interpolate : /\{\{(.+?)\}\}/g
   };
 
+  var spinner = new Spinner({
+    lines: 5,
+    length: 6,
+    width: 5, 
+    radius: 0,
+    corners: 1.0,
+    rotate: 0,
+    trail: 62,
+    speed: 0.9 
+  });
+
+  $("body").ajaxStart(function() { spinner.spin($(".loading").get(0)); });
+  $("body").ajaxStop(function () { spinner.stop($(".loading").get(0)); });
+
 })();
--- app/templates/pages/photos.hb
+++ app/templates/pages/photos.hb
@@ -3,6 +3,7 @@
 	  <legend>Let's find some Photos...</legend>
 	  <input type="text" placeholder="Search Flickr...">
     <button type="submit" class="btn">Submit</button>
+    <div class="loading"></div>
 	</form>
 
   <ul id="photo-viewer" class="unstyled"></ul>

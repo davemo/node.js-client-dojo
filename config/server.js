8646c03fde041cd9432911d0d2a1abe1bab76bae
David Mosher <davidmosher@gmail.com>
2012-09-21 13:47:03 -0600

    added our custom express server, updated the default grunt config so that it launches our server instead of its own, updated our package.json with our new dependencies

---
+++ config/server.js
@@ -0,0 +1,28 @@
+var express            = require('express'), 
+    request            = require('request'),
+    app                = express.createServer(),
+    FLICKR_KEY      = "dc43133eacbf9aa77ba291e2893c9caa",
+    FLICKR_SECRET   = "f2e707741f9d4cf1",
+    FLICKR_API_ROOT = "http://api.flickr.com/services/rest/?per_page=30&nojsoncallback=1&format=json&api_key=" + FLICKR_KEY;
+
+app.configure(function() {
+  app.use(express.static(process.cwd() + "/generated"));
+  app.use(express.bodyParser());
+  app.use(express.errorHandler());
+});
+
+app.get('/proxy/photos/:tags', function(req, res) { // /proxy/photos/cows
+  // http://www.flickr.com/services/api/flickr.photos.search.html
+  var uri = FLICKR_API_ROOT + "&tags=" + req.params.tags + "&method=flickr.photos.search";
+      
+  request.get(uri, function(error, response, body) {
+    if(!error && response.statusCode == 200) {
+      res.contentType('application/json');
+      res.send(body);
+    } else {
+      res.send("flickr proxy error: " + error);
+    }
+  });
+});
+
+module.exports = app;
--- grunt.js
+++ grunt.js
@@ -1,6 +1,7 @@
 /*global module:false*/
 module.exports = function(grunt) {
   var _ = grunt.utils._,
+      server = require(process.cwd() + '/config/server.js'),
       appTasks = require(process.cwd() + '/config/application.js').appTasks;
 
   grunt.loadTasks('tasks');
@@ -11,4 +12,9 @@ module.exports = function(grunt) {
   grunt.registerTask('run', _.union(appTasks.common, appTasks.watch).join(' '));
 
   grunt.task.run('configure');
+
+  grunt.registerTask('server', 'Custom Express Server', function() {
+    grunt.log.writeln('Starting express web server in "./generated" on port 3000');
+    server.listen(3000);
+  });
 };
